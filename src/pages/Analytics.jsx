3a0f808e6f054f12c7c811fa02e8b4a83c1a26bf
Timothy <timothy.mureithi@student.moringaschool.com>
2023-01-16 23:40:46 +0300

    side-nav

---
+++ src/pages/Analytics.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from './components/Sidebar';
+
+const Analytics = () => {
+    return (
+        <div>
+            <h1>Analytics page</h1>
+        </div>
+    );
+};
+
+export default Analytics;
